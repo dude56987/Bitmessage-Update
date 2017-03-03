c1d3c083e105e96cf8015acbbc7f68281104f221
dude56987 <dude56987@users.noreply.github.com>
2017-03-03 16:43:44 -0500

    Added header to bitmessage-update.sh

--- bitmessage-update.sh
+++ bitmessage-update.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
+########################################################################
+# Script to update PyBitmessage from the source git repository
+# Copyright (C) 2017  Carl J Smith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################
 if [ -d /opt/PyBitmessage ];then
 	echo 'Upgrading current install of PyBitmessage...';
 	# checkout the files with unmodified permissions for update
